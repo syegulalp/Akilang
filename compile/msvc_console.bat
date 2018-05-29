005d3d732482382b197c0dcafd97e5508a09e43f
Serdar Yegulalp <serdar@genjipress.com>
2018-05-29 19:26:56 -0400

    remove old batch file compilation mechanism

--- compile/msvc_console.bat
+++
@@ -1,4 +0,0 @@
-@SETLOCAL
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-link.exe %1.obj -defaultlib:ucrt msvcrt.lib user32.lib kernel32.lib legacy_stdio_definitions.lib /SUBSYSTEM:CONSOLE /MACHINE:X64 /OUT:%1.exe /OPT:REF
-EXIT /B %ERRORLEVEL%
