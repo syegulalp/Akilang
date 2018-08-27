54e0ea6c6c9b122710dd4e28818f005d27705516
Serdar Yegulalp <serdar@genjipress.com>
2018-08-27 17:07:36 -0400

    cleanup

--- core/stdlib/nt.py
+++ core/stdlib/nt.py
@@ -1,20 +1,27 @@
+import llvmlite.ir as ir
+from core.mangling import mangle_function
+
+def makefunc(module, func_name, func_type, func_sig):
+    func_s = ir.FunctionType(func_type, func_sig)
+    func = mangle_function(ir.Function(module, func_s, func_name))
+    func.attributes.add('nonlazybind')
+    func.linkage='private'
+    irbuilder = ir.IRBuilder(func.append_basic_block('entry'))
+    return func, irbuilder
+
 def stdlib(self, module):
 
-    # TODO: emit all this as bitcode
+    # TODO: emit all this as bitcode, save it
 
-    import llvmlite.ir as ir
-    
     from core.vartypes import VarTypes
-    from core.mangling import mangle_function
-
+    
     # string length
-    # eventually when we move string to an identified type, we can remove this.
 
-    strlen_s = ir.FunctionType(VarTypes.u32, [VarTypes.str.as_pointer()])
-    strlen = mangle_function(ir.Function(module, strlen_s, '.object.str.__len__'))
-    strlen.attributes.add('nonlazybind')
-    strlen.linkage='private'
-    irbuilder = ir.IRBuilder(strlen.append_basic_block('entry'))
+    strlen, irbuilder = makefunc(
+        module,
+        '.object.str.__len__',
+        VarTypes.u32, [VarTypes.str.as_pointer()]
+    )
 
     # extract element 0, the length
 
@@ -26,38 +33,3 @@ def stdlib(self, module):
     s3 = irbuilder.load(s2)
 
     irbuilder.ret(s3)
-
-    # type builders:
-    # int() to: str(), float()
-    # str() to: int(), float()
-    # float() to: int(), str()
-    # number-to-number conversions should be codegenned as a cast
-    # eventually create a grid
-
-    '''
-    One of the problems we have now:
-    how do we deal with the stuff that's codegenned in advance, programmatically,
-    vs. the stuff we have to codegen by way of library functions?
-    what prevents us from doing everything as library functions?
-    we need to find out what those things are.
-    barring that, we need a way to more efficiently generate programmatic functions
-    that we could also use to produce the casts between types we need here
-    '''
-    
-    # for n in VarTypes:
-    #     if isinstance(VarTypes[n], ir.types.IntType):
-        
-    # mkstr_s = ir.FunctionType(VarTypes.i32, [VarTpes.str.as_pointer()])
-    # mkstr = mangle_function(ir.Function(module, mkstr_s, '.i32.__str__'))
-
-    # mkstr.attributes.add('nonlazybind')
-    # mkstr.linkage='private'
-    # irbuilder = ir.IRBuilder(mkstr.append_basic_block('entry'))
-
-    # construct a call to snprintf
-    # get syntax for that
-    # and print into the buffer
-    # then construct the new object
-
-    # each int type should be converted to either signed or unsighned i64 first,
-    # and then we call based on that?
