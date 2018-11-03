200feb756788f689430c7b36fc6533c85b5463bb
Serdar Yegulalp <serdar@genjipress.com>
2018-11-03 11:27:12 -0400

    filling out codewalker

--- core/walker.py
+++ core/walker.py
@@ -1,20 +1,38 @@
 from core.parsing import Parser
 
+class Function():
+    def __init__(self):
+        self.variables = {}
 
 class Walker():
     def __init__(self, code):
         self.walker = Parser().parse_generator(code)
+        self.functions = {}
 
-    def walk(self):
-        for node in self.walker:
+    def walk(self, walker=None):
+        if walker is None:
+            walker = self.walker
+        for node in walker:
             method = f'_walk_{node.__class__.__name__}'
-            result = getattr(self, method)(node)
-            print(result)
+            result = getattr(self, method)(node)            
+            return result
 
     def _walk_Function(self, node):
-        for statement in node.body.expr_list:
-            print(statement.__class__)
+        self.functions[node.proto.name] = Function()
+        return self.walk(node.body.expr_list)
 
+    def _walk_Var(self, node):
+        # these are variable declarations
+        return self.walk(node.vars)
+    
+    def _walk_Call(self, node):
+        pass
+
+    def _walk_Number(self, node):
+        pass
+
+    def _walk_Variable(self, node):
+        return node.name
 
 if __name__ == '__main__':
 
@@ -29,4 +47,12 @@ if __name__ == '__main__':
     }
     '''
 
-    Walker(code).walk()
+    w= Walker(code)
+    w.walk()
+    print (w.functions)
+
+# find out whether or not a given variable is ever given away
+# if so, is it given away to a @nomod function?
+# is it returned from a @nomod function? (as a call result)
+# if its owner remains consistent, then we heap allocate
+# if not, we stack allocate 
