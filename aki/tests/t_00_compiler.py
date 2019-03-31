16c5c0c788751fa2ff445dc7363377c56e175653
Serdar Yegulalp <serdar@genjipress.com>
2019-03-31 00:17:47 -0400

    note to self for future

--- aki/tests/t_00_compiler.py
+++ aki/tests/t_00_compiler.py
@@ -1,3 +1,6 @@
 # Test compiler by having it compile all example apps.
 # This is a good way to fail fast if we break something major,
 # especially since our examples all need to compile.
+
+# This should also include module load and name cross-linking tests,
+# e.g. the `g1()+g1()` test
