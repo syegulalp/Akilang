829481318637e2c1cceed2f92f3e7a5e27eb3930
Serdar Yegulalp <serdar@genjipress.com>
2019-04-11 22:44:14 -0400

    format cleanup

--- aki/core/constants.py
+++ aki/core/constants.py
@@ -1,27 +1,34 @@
 PRODUCT = "Aki"
-VERSION = '0.0.2019.04.10'
+VERSION = "0.0.2019.04.10"
 COPYRIGHT = "2019"
 
-ABOUT = f'''{PRODUCT} v.{VERSION}
+WELCOME = f"{PRODUCT} v.{VERSION}"
+
+ABOUT = f"""{WELCOME}
 © {COPYRIGHT} Serdar Yegulalp
 
 Based on code created by:
 - Frédéric Guérin (https://github.com/frederickjeanguerin/pykaleidoscope)
-- Eli Bendersky (https://github.com/eliben/pykaleidoscope)'''
+- Eli Bendersky (https://github.com/eliben/pykaleidoscope)"""
 
-WELCOME = f"{PRODUCT} v.{VERSION}"
 
 def defaults():
     return {
-        'paths': {
-            'source_dir':'examples',
-            'output_dir':'output',
-            'dump_dir':'.',
-            'nt_compiler': "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat"
+        "paths": {
+            "source_dir": "examples",
+            "output_dir": "output",
+            "dump_dir": ".",
+            "nt_compiler": "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat",
         },
-        'settings': {
-            'write_main_to_file': {'Dumps loaded module LLVM IR to "{paths.output_dir}" on load.', True},
-            'write_repl_to_file': {'Dumps REPL LLVM IR to {paths.output_dir}" on load.', True},
-            'compile_on-load': {'Compile immediately when a file is loaded.', True},
-        }
+        "settings": {
+            "write_main_to_file": {
+                'Dumps loaded module LLVM IR to "{settings.paths.output_dir}" on load.',
+                True,
+            },
+            "write_repl_to_file": {
+                'Dumps REPL LLVM IR to "{setting.paths.output_dir}" on load.',
+                True,
+            },
+            "compile_on-load": {"Compile immediately when a file is loaded.", True},
+        },
     }
